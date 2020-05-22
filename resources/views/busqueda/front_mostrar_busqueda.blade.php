b45a5c4a788bf7daeeb5e00069510059731a1340
DiegoZamora33 <61322779+DiegoZamora33@users.noreply.github.com>
2020-05-21 23:46:31 -0500

    Update front_mostrar_busqueda.blade.php

--- resources/views/busqueda/front_mostrar_busqueda.blade.php
+++ resources/views/busqueda/front_mostrar_busqueda.blade.php
@@ -15,7 +15,7 @@
   <div class="card mx-auto w-75 mb-4">
       <div class="card btn btn-light p-0 text-left" id="{{ $entrenador->idEntrenador }}" onclick="getEntre(this);">
          <input type="hidden" name="_token" value="{{ csrf_token() }}" id="token_{{ $entrenador->idEntrenador }}">
-         <h6 class="card-header text-white bg-secondary mb-0 font-weight-bold">Entrenador</h6>
+         <h6 class="card-header text-white bg-info mb-0 font-weight-bold">Entrenador</h6>
          <div class="card-text mt-1 pl-2 pr-2 d-flex"><h6 class="font-weight-bold mr-2">{{ $entrenador->nombre }} {{ $entrenador->apellidoPaterno }} {{ $entrenador->apellidoMaterno }}</h6></div>
          <div class="card-text pl-2 pr-2 d-flex"><h6 class="mr-2">Fecha de Registro:</h6><h6>{{ substr(str_limit($entrenador->created_at, $limit = 10, $end = " "),8,2)."/".substr(str_limit($entrenador->created_at, $limit = 10, $end = " "),5,2)."/".substr(str_limit($entrenador->created_at, $limit = 10, $end = " "),0,4)}}</h6></div>
       </div>
